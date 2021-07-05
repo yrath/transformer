192deb386dacbb51c12552c0c86ca83dd743eb70
Yannik <yannik.alexander.rath@cern.ch>
2021-07-05 20:15:55 +0200

    Add prediction function, disregard padded values in loss.

--- transformer/main.py
+++ transformer/main.py
@@ -36,8 +36,10 @@ class Collater(object):
     def __call__(self, data_batch):
         de_batch, en_batch = [], []
         for (de_item, en_item) in data_batch:
+            de_item = de_item.strip().strip(".")
+            en_item = en_item.strip().strip(".")
             de_tensor = torch.tensor(self.de_vocab(self.de_tokenizer(de_item)))
-            en_tensor = torch.tensor(self.en_vocab(self.en_tokenizer(de_item)))
+            en_tensor = torch.tensor(self.en_vocab(self.en_tokenizer(en_item)))
             de_batch.append(torch.cat([torch.tensor([self.de_bos]), de_tensor, torch.tensor([self.de_eos])], dim=0))
             en_batch.append(torch.cat([torch.tensor([self.en_bos]), en_tensor, torch.tensor([self.en_eos])], dim=0))
 
@@ -47,7 +49,32 @@ class Collater(object):
         return de_batch, en_batch
 
 
-def train_epoch(model, optimizer, trainloader, device):
+def translate(model, loader, de_vocab, en_vocab, device, max_len=200):
+    for src_sentence, target_sentence in loader:
+        print("Sentence to translate:")
+        print(" ".join([de_vocab.lookup_token(token_idx) for token_idx in src_sentence[0]]))
+
+        src_sentence = src_sentence.to(device)
+        pred_sentence = torch.tensor([en_vocab["<bos>"]])
+        for _ in range(max_len):
+            seq_size = pred_sentence.size()[0]
+            subsequent_mask = torch.tril(torch.ones(seq_size, seq_size)).to(device)
+
+            pred = model(src_sentence, pred_sentence.unsqueeze(0).to(device), subsequent_mask)
+
+            next_word = torch.argmax(F.softmax(pred[:,-1,:], dim=-1))
+
+            pred_sentence = torch.cat([pred_sentence, next_word.unsqueeze(0)])
+            if next_word == en_vocab["<eos>"]:
+                break
+
+        print("Result:")
+        print(" ".join([en_vocab.lookup_token(token_idx) for token_idx in pred_sentence]))
+        print("Target:")
+        print(" ".join([en_vocab.lookup_token(token_idx) for token_idx in target_sentence[0]]))
+
+
+def train_epoch(model, optimizer, trainloader, device, ignore_index):
     model.train()
     total_loss = 0
     for src, target in trainloader:
@@ -61,7 +88,8 @@ def train_epoch(model, optimizer, trainloader, device):
         target_output = target[:, 1:]
         pred = model(src, target_input, subsequent_mask)
 
-        loss = F.cross_entropy(pred.view(-1, pred.size(-1)), target_output.contiguous().view(-1))
+        loss = F.cross_entropy(pred.view(-1, pred.size(-1)), target_output.contiguous().view(-1),
+            ignore_index=ignore_index)
         print(loss)
 
         loss.backward()
@@ -115,10 +143,15 @@ def main(rebuild_vocab=False):
         train_dataset = torchtext.datasets.IWSLT2017(root=data_dir, split="train", language_pair=("de", "en"))
         trainloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, collate_fn=collater)
         try:
-            training_loss = train_epoch(model, optimizer, trainloader, device)
+            training_loss = train_epoch(model, optimizer, trainloader, device, en_vocab["<pad>"])
             print(f"Epoch {i_epoch}: {training_loss:.3f}")
         except KeyboardInterrupt:
             break
+
+    model.eval()
+    valid_dataset = torchtext.datasets.IWSLT2017(root=data_dir, split="train", language_pair=("de", "en"))
+    validloader = DataLoader(valid_dataset, batch_size=1, collate_fn=collater)
+    translate(model, validloader, de_vocab, en_vocab, device)
 
 if __name__ == "__main__":
     main()
